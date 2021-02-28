fd9142e3a306ccaab15bbba855983ea8cb7591d8
NitzanHen <nitzan.alpha@gmail.com>
2021-03-01 00:06:38 +0200

    Fix bug with Functor<{}> returning never

--- src/Functor.ts
+++ src/Functor.ts
@@ -2,8 +2,16 @@ import { CoreFunctor } from './CoreFunctor';
 import { NumberFunctor } from './NumberFunctor';
 import { ObjectFunctor } from './ObjectFunctor';
 
+
 export type Functor<T> =
   & CoreFunctor<T>
   & T extends number ? NumberFunctor : {}
-  & T extends Record<infer K, infer V> ? K extends string ? ObjectFunctor<K, V> : {} : {};
-
+  & {} extends T
+  ? ObjectFunctor<never, never>
+  /** For some reason, an empty object makes this conditional clause "crash" and automatically return never.
+   *  The check above for {} guards this - returning {} immediately. */
+  : T extends Record<infer K, infer V>
+  /**/ ? K extends string
+  /****/ ? ObjectFunctor<K, V>
+  /****/ : {}
+  /**/ : {};
