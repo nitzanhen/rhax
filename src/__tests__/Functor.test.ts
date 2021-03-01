75d41cfe80c46f9df4c3965c0930486f215d27d9
NitzanHen <nitzan.alpha@gmail.com>
2021-03-01 21:09:12 +0200

    add temp test

--- src/__tests__/Functor.test.ts
+++ src/__tests__/Functor.test.ts
@@ -1,7 +1,14 @@
+import { take } from '../CoreFunctor';
 import { Functor } from '../Functor';
 
 describe("Functor", () => {
-  test("Sanity test", () => {
-    expect(true).toBe(true);
+  test("temp", () => {
+    expect(
+      take({ a: 3, b: 4, c: 5 })
+        .reduce((acc, val) => acc + val, 0)
+        .map(x => 2 * x - 5)
+        .default(-7)
+        ()
+    ).toBe(19)
   })
 })
