6d58f7602008813c04605a1cbd2ca2e6ab797e89
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-04-27 08:55:55 +0000

    temp benchmark tests

--- test/unit/temp.test.ts
+++ test/unit/temp.test.ts
@@ -1,13 +1,24 @@
-import { take } from 'RhaxCore';
+import { Rhax, take } from 'Rhax';
+
 
 describe('temp', () => {
   test('tempt', () => {
     const val = 3;
     const start = process.hrtime.bigint();
-    for (let i = 0; i < 10_000; i++) {
-      //take(val)();
+    for (let i = 0; i < 1_000_000; i++) {
+      take(val)();
     }
     const end = process.hrtime.bigint();
-    console.log(`Benchmark took ${(end - start) / 1_000_000n} milliseconds`);
+    console.log(`Current benchmark took ${(end - start) / 1_000_000n} milliseconds`);
+  });
+
+  test('tempt2', () => {
+    const val = 3;
+    const start = process.hrtime.bigint();
+    for (let i = 0; i < 1_000_000; i++) {
+      new Rhax(val)();
+    }
+    const end = process.hrtime.bigint();
+    console.log(`New benchmark took ${(end - start) / 1_000_000n} milliseconds`);
   });
 });
