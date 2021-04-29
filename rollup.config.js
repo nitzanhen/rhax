8245663cd3d930cb5b14216fccd24dc1206fc718
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-04-29 12:08:47 +0000

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+
+import pkg from './package.json';
+
+/**
+ * Based on config at
+ * https://gist.github.com/aleclarson/9900ed2a9a3119d865286b218e14d226
+ * 
+ * @todo research mjs format output (configured in the link above) - advantages, popularity, etc.
+ */
+
+const name = pkg.main.replace(/\.js$/, '');
+
+const bundle = config => ({
+  ...config,
+  input: 'src/index.ts',
+  //external: id => !/^[./]/.test(id),
+});
+
+export default [
+  bundle({
+    plugins: [esbuild()],
+    output: [
+      {
+        file: `${name}.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es',
+    },
+  }),
+];
