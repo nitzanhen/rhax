611ffae65340f463100ccf83b68c6b6759afdf8f
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-05-04 10:36:56 +0000

    object groupBy styling

--- src/fp/object/groupBy.ts
+++ src/fp/object/groupBy.ts
@@ -3,7 +3,10 @@ import { ValueOf } from '../../utils/types';
 import { reduce } from './reduce';
 
 export const groupBy = <O, T extends string | number | symbol>(tagger: (value: ValueOf<O>, key: keyof O, record: O) => T, record: O) =>
-  reduce((groups, v, k, record) => {
-    const tag = tagger(v, k, record);
-    return { ...groups, [tag]: groups[tag] ? [...groups[tag]!, v] : [v] };
-  }, {} as Partial<Record<T, ValueOf<O>[]>>, record);
+  reduce(
+    (groups, v, k, record) => {
+      const tag = tagger(v, k, record);
+      return { ...groups, [tag]: groups[tag] ? [...groups[tag]!, v] : [v] };
+    },
+    {} as Partial<Record<T, ValueOf<O>[]>>, record
+  );
