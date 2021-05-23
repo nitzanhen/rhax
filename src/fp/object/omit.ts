e8768b2ab7ab729630c42b73e0b44ee150cc2c16
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-05-23 11:39:08 +0000

    Add Set casting to pick & omit keys arg

--- src/fp/object/omit.ts
+++ src/fp/object/omit.ts
@@ -1,8 +1,11 @@
 import { reduce } from './reduce';
 
-export const omit = <O, K extends keyof O>(keys: K[], record: O): Omit<O, K> =>
-  reduce((acc, v, k) =>
-    (keys as (keyof O)[]).includes(k) ? acc : ({ ...acc, [k]: v }),
+export const omit = <O, K extends keyof O>(keys: K[], record: O): Omit<O, K> => {
+  const keySet = new Set<keyof O>(keys);
+
+  return reduce((acc, v, k) =>
+    keySet.has(k) ? acc : ({ ...acc, [k]: v }),
     {} as Omit<O, K>,
     record
   );
+};
--- src/fp/object/pick.ts
+++ src/fp/object/pick.ts
@@ -1,8 +1,12 @@
 import { reduce } from './reduce';
 
-export const pick = <O, K extends keyof O>(keys: K[], record: O): Pick<O, K> =>
-  reduce((acc, v, k) =>
-    (keys as (keyof O)[]).includes(k) ? ({ ...acc, [k]: v }) : acc,
+export const pick = <O, K extends keyof O>(keys: K[], record: O): Pick<O, K> => {
+  const keySet = new Set<keyof O>(keys);
+
+  return reduce((acc, v, k) =>
+    keySet.has(k) ? ({ ...acc, [k]: v }) : acc,
     {} as Pick<O, K>,
     record
   );
+};
+
