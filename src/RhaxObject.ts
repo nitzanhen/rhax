1d752784936909a62c2cd88f0db68afc4d91f6f4
NitzanHen <nitzan.alpha@gmail.com>
2021-05-28 20:57:19 +0000

    unset abstract modifier on type classes

--- src/RhaxObject.ts
+++ src/RhaxObject.ts
@@ -7,7 +7,7 @@ import { RhaxBase } from './RhaxBase';
 import { ValueOf } from './utils/types';
 
 export interface RhaxObject<O> extends RhaxBase<O> { }
-export abstract class RhaxObject<O> {
+export class RhaxObject<O> {
 
   mapFields<W>(fn: (value: ValueOf<O>, key: keyof O, record: O) => W) {
     return rhax(mapFields(fn, this.value));
