e0c181df2bd06908c8a41380d89eb3adce173592
Nitzan Hen <nitzan.alpha@gmail.com>
2021-07-27 16:33:54 +0300

    add dualFunc

---
+++ src/utils/dualFunc.ts
@@ -0,0 +1,26 @@
+import { curryLast } from "./curryLast";
+import { Func, NoTail, Tail } from "./types";
+
+export interface DualFunc<A extends any[], B> {
+    (...args: A): B;
+    (...args: NoTail<A>): (...tail: Tail<A>) => B
+}
+
+export function dualFunc<A extends any[], B>(
+    fn: Func<A, B>
+) {
+    const curried = curryLast(fn);
+
+    // Define the returned function as an anonymous function in an object
+    // to preserve its name, then return that function.
+    const returns = {
+        [fn.name]: function (...args: [...NoTail<A>, Tail<A>?]) {
+            const tailFn = curried(...(args as any))
+            return arguments.length >= fn.length
+                ? (tailFn as any)(arguments[fn.length])
+                : tailFn
+        } as unknown as DualFunc<A, B>
+    };
+
+    return Object.values(returns)[0];
+}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,4 +2,10 @@ export type ValueOf<T> = T[keyof T];
 
 export type EmptyObj = Record<string, never>;
 
-export type Constructor<T> = new (...args: any[]) => T
+export type Constructor<T> = new (...args: any[]) => T;
+
+export type Func<A extends any[], B> = (...args: A) => B;
+
+export type NoTail<A extends any[]> = A extends [...(infer H), any] ? H : never;
+export type Rest<H extends any[], A extends [...H, ...any[]]> = A extends [...H, ...(infer T)] ? T : never;
+export type Tail<A extends any[]> = A extends [...(infer H), any] ? Rest<H, A> : never;
