653a7980d7965834766f587072200de0a7e83f34
NitzanHen <nitzan.alpha@gmail.com>
2021-08-01 22:48:11 +0000

    start reforming functions

--- src/fp/object/filter.ts
+++ src/fp/object/filter.ts
@@ -1,10 +1,28 @@
-import { ValueOf } from '../../utils/types';
+import { RecordKey, ValueOf } from '../../utils/types';
 
-export const filter = <O>(
-  predicate: (value: ValueOf<O>, key: keyof O, record: O) => boolean, record: O
-): Partial<O> =>
-  (Object.entries(record) as [keyof O, ValueOf<O>][])
+export function filter<K extends RecordKey, V>(
+  predicate: (value: V, key: K) => boolean
+): <O extends Record<K, V>>(record: O) => Partial<O>;
+
+export function filter<O>(
+  predicate: (value: ValueOf<O>, key: keyof O) => boolean
+): (record: O) => Partial<O>;
+
+export function filter<O>(
+  predicate: (value: ValueOf<O>, key: keyof O) => boolean,
+  record: O
+): Partial<O>;
+
+export function filter<O>(
+  predicate: (value: ValueOf<O>, key: keyof O) => boolean, record?: O
+) {
+  if (record === undefined) {
+    return (record: O) => filter(predicate, record);
+  }
+
+  return (Object.entries(record) as [keyof O, ValueOf<O>][])
     //Filter entries by the predicate
-    .filter(([k, v]) => predicate(v, k, record))
+    .filter(([k, v]) => predicate(v, k))
     //Reduce the entries array back to an object
     .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {} as Partial<O>);
+}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -9,3 +9,5 @@ export type Func<A extends any[], B> = (...args: A) => B;
 export type NoTail<A extends any[]> = A extends [...(infer H), any] ? H : never;
 export type Rest<H extends any[], A extends [...H, ...any[]]> = A extends [...H, ...(infer T)] ? T : never;
 export type Tail<A extends any[]> = A extends [...(infer H), any] ? Rest<H, A> : never;
+
+export type RecordKey = string | number | symbol;
