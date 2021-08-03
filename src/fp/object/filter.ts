7cfe3f13dd68403a2dec7f656eb284ad232b5f77
NitzanHen <nitzan.alpha@gmail.com>
2021-08-03 20:36:51 +0000

    try filter overloading

--- src/fp/object/filter.ts
+++ src/fp/object/filter.ts
@@ -1,28 +1,53 @@
 import { RecordKey, ValueOf } from '../../utils/types';
 
+type RecordPredicate<O> = (value: ValueOf<O>, key: keyof O) => boolean;
+type ArrayPredicate<E> = (value: E, index: number) => boolean;
+
+//------ Record overloads ------//
+
 export function filter<K extends RecordKey, V>(
   predicate: (value: V, key: K) => boolean
 ): <O extends Record<K, V>>(record: O) => Partial<O>;
 
 export function filter<O>(
-  predicate: (value: ValueOf<O>, key: keyof O) => boolean
+  predicate: RecordPredicate<O>
 ): (record: O) => Partial<O>;
 
 export function filter<O>(
-  predicate: (value: ValueOf<O>, key: keyof O) => boolean,
+  predicate: RecordPredicate<O>,
   record: O
 ): Partial<O>;
 
-export function filter<O>(
-  predicate: (value: ValueOf<O>, key: keyof O) => boolean, record?: O
+//------ Arrau overloads ------//
+
+export function filter<E>(
+  predicate: ArrayPredicate<E>
+): (arr: E[]) => E[];
+
+export function filter<E>(
+  predicate: ArrayPredicate<E>,
+  arr: E[]
+): E[];
+
+export function filter<O, E>(
+  predicate: RecordPredicate<O> | ArrayPredicate<E>,
+  recordOrArray?: O | E[]
 ) {
-  if (record === undefined) {
-    return (record: O) => filter(predicate, record);
+
+  if (recordOrArray === undefined) {
+    return (collection: O | E[]) => filter(predicate as any, collection);
   }
 
-  return (Object.entries(record) as [keyof O, ValueOf<O>][])
+  if (Array.isArray(recordOrArray)) {
+    return recordOrArray.filter(predicate as ArrayPredicate<E>);
+  }
+
+  const entries = Object.entries(recordOrArray) as [keyof O, ValueOf<O>][];
+  return entries
     //Filter entries by the predicate
-    .filter(([k, v]) => predicate(v, k))
+    .filter(([k, v]) => (predicate as RecordPredicate<O>)(v, k))
     //Reduce the entries array back to an object
     .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {} as Partial<O>);
 }
+
+filter<number[]>((n, i) => n + i > 5, )
