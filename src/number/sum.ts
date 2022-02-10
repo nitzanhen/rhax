e0a3373970bf6294039e10888d8413d60ad7da89
Nitzan Hen <code@nitzanhen.net>
2022-02-10 03:19:13 +0200

    add sum overloads

--- src/number/sum.ts
+++ src/number/sum.ts
@@ -2,5 +2,13 @@
 export const sumItems = <T>(items: T[], toNumber: (it: T) => number): number =>
   items.map(toNumber).reduce((s, x) => s + x, 0);
 
-export const sum = (...numbers: number[]): number => 
-  numbers.reduce((s, x) => s + x, 0);
+export function sum(...numbers: number[]): number;
+export function sum(numbers: number[]): number;
+export function sum(...args: any[]): number {
+  if (args.length > 1 || !Array.isArray(args[0])) {
+    return sum(args);
+  }
+  const [numbers] = args;
+
+  return numbers.reduce((s, x) => s + x, 0);
+}
