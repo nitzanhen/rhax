71fe516c04126467fcaa75dcc4b4faf971c27340
Nitzan Hen <code@nitzanhen.net>
2022-02-10 03:37:50 +0200

    Add pipe overload with no args

--- src/pipe.ts
+++ src/pipe.ts
@@ -5,8 +5,13 @@ export interface Pipe<T> {
   go(): T
 }
 
-export const pipe = <T>(x: T): Pipe<T> => {
+export interface PipeFactory {
+  (): Pipe<void>;
+  <T>(x: T): Pipe<T>;
+}
+
+export const pipe = (<T>(x: T): Pipe<T> => {
   const cb = <S>(fn: Fn<T, S>) => pipe(fn(x)) as unknown as Pipe<S>;
   cb.go = () => x;
   return cb;
-};
+}) as PipeFactory;
