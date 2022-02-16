3fb82fc57f26add5c0fba5c55ba69976750f8684
Nitzan Hen <code@nitzanhen.net>
2022-02-16 17:16:45 +0200

    Change sum() to const

--- src/number/sum.ts
+++ src/number/sum.ts
@@ -2,13 +2,16 @@
 export const sumItems = <T>(items: T[], toNumber: (it: T) => number): number =>
   items.map(toNumber).reduce((s, x) => s + x, 0);
 
-export function sum(...numbers: number[]): number;
-export function sum(numbers: number[]): number;
-export function sum(...args: any[]): number {
+export interface SumFunction {
+  (...numbers: number[]): number;
+  (numbers: number[]): number;
+}
+
+export const sum: SumFunction = ((...args: any[]): number => {
   if (args.length > 1 || !Array.isArray(args[0])) {
     return sum(args);
   }
   const [numbers] = args;
 
   return numbers.reduce((s, x) => s + x, 0);
-}
+});
