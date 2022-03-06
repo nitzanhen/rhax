b602aedc2a10be55adca7299d57fa19e3b546569
Nitzan Hen <code@nitzanhen.net>
2022-03-06 12:23:09 +0200

    Avoid desctructured named params on clamp

--- src/number/clamp.ts
+++ src/number/clamp.ts
@@ -1,6 +1,6 @@
 
 /**
- * Ensures that `x` is in the range between the given minimum and maximum values.
+ * Ensures that `x` is within the given interval.
  * If it isn't, returns the closest bound (`min` if `x < min`, `max` if `max < x`) 
  * 
  * @example
@@ -8,8 +8,8 @@
  * clamp(-1, [0, 2]) // ==> 0
  * clamp(3, [0, 2])  // ==> 2
  */
-export function clamp(x: number, [min, max]: [number, number]): number;
-export function clamp([min, max]: [number, number]): (x: number) => number;
+export function clamp(x: number, interval: [min: number, max: number]): number;
+export function clamp(interval: [min: number, max: number]): (x: number) => number;
 export function clamp(...args: any[]) {
   if (args.length === 1) {
     const [[min, max]] = args;
