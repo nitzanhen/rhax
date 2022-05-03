44be5cad05ab80e07aa00e71f09355eb24182f2f
Nitzan Hen <code@nitzanhen.net>
2022-05-03 21:55:46 +0300

    resultify fix

--- src/result/AsyncResult.ts
+++ src/result/AsyncResult.ts
@@ -2,4 +2,9 @@ import { err, ok, Result } from './Result';
 
 export type AsyncResult<T, E = unknown> = Promise<Result<T, E>>;
 
-export const resultify = <T, E = unknown>(promise: Promise<T>): AsyncResult<T, E> => promise.then(ok).catch(err);
+export const resultify = <T, E = unknown>(promise: Promise<T>): AsyncResult<T, E> => new Promise((resolve, reject) => {
+  promise.then(
+    v => resolve(ok(v)),
+    e => reject(err(e))
+  );
+});
