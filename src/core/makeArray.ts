ef97d9b4941bf7e6c9c96b70920204ab9a584891
Nitzan Hen <code@nitzanhen.net>
2022-06-22 15:53:54 +0300

    Add makeArray override with default factory

--- src/core/makeArray.ts
+++ src/core/makeArray.ts
@@ -4,10 +4,15 @@
  * 
  * @param len new array's length.
  * @param factory array element factory.
+ * if `factory` is not set, the created array will be filled with the indices.
  * 
  * @example 
  * const arr = makeArray(4, i => i ** 2);
  * console.log(arr); // [1, 4, 9 ,16] 
  */
-export const makeArray = <T>(len: number, factory: (index: number) => T): T[] =>
-  [...new Array(len).keys()].map(factory);
+export function makeArray(len: number): number[];
+export function makeArray<T>(len: number, factory: (index: number) => T): T[];
+export function makeArray(len: number, factory: (index: number) => any = (i => i)) {
+  return [...new Array(len).keys()].map(factory);
+}
+  
--- test/core/makeArray.test.ts
+++ test/core/makeArray.test.ts
@@ -3,7 +3,9 @@ import { makeArray } from 'core';
 describe('makeArray', () => {
   test('makeArray', () => {
     expect(makeArray(0, i => i)).toEqual([]);
+    expect(makeArray(0)).toEqual([]);
     expect(makeArray(3, i => i)).toEqual([0, 1, 2]);
+    expect(makeArray(3)).toEqual([0, 1, 2]);
 
     expect(makeArray(5, () => ({}))).toEqual([{}, {}, {}, {}, {}]);
   });
