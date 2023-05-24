a7bca2f65ccef5c2ee8e9a0aae43b4725adf70b3
Nitzan Hen <nitzan.hen@agamimp.net>
2023-05-24 11:38:21 +0300

    Add empty `ok()` overload

--- src/result/Result.ts
+++ src/result/Result.ts
@@ -2,8 +2,10 @@ import { ErrLike, OkLike } from './ResultLike';
 
 export type Ok<T> = OkLike<{ data: T }>;
 
-export function ok<T>(data: T): Ok<T> {
-  return { ok: true, data };
+export function ok(): Ok<void>;
+export function ok<T>(data: T): Ok<T>;
+export function ok<T>(data?: T): Ok<T> {
+  return { ok: true, data: data as T };
 }
 
 export type Err<E> = ErrLike<{ error: E }>;
